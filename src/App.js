f46b8d38f8c21104970ad8a8fe46cdb2bac04478
PanDamko <knapdam@gmail.com>
2023-07-17 21:03:14 +0200

    usestate

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import { useState } from 'react';
 import './App.css';
 
 import Answer from './components/Answer';
 import Question from './components/Question';
 
 function App() {
+const [counter, setCounter] =useState(0);
+const [widze, setWidze] =useState(true);
+const handleIncrease = () =>{
+setCounter(counter+1)
+}
+//   let counter = 0;
+//   const incCounter = ()=>{counter++;
+//   console.log("test");
+// console.log(counter);}
   return (
     <div className='App'>
       <div className='column'>
+        {/* <h2>{counter}</h2>
+        <button onClick={incCounter}>Zwiększ o 1</button> */}
+        <h2>{counter}</h2>
+        <button onClick={()=>setWidze(!widze)}>{widze ? "widać" : "Nie widać"}</button>
+        <button onClick={handleIncrease}>Zwiększ o 1</button>
+        <button onClick={()=>{setCounter(counter-1)}}>Zwiększ o 1</button>
         <Question />
-        <Answer />
+        {widze ? <Answer /> : ""}
+        
       </div>
     </div>
   );
