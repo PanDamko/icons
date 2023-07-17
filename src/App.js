a0dd4fcfed05f0bb97972d0a96ae9cb64924c7ad
PanDamko <knapdam@gmail.com>
2023-07-17 18:54:44 +0200

    ansewer no

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import { FaBeer } from 'react-icons/fa';
+import Answer from './components/Answer';
 
 function App() {
   return (
     <div className='App'>
-    <h1> Lets go for a <FaBeer />? </h1>
+      <div className='column'>
+        <h1> Lets go for a <FaBeer />? </h1>
+        <Answer />
+      </div>
     </div>
   );
 }
---
+++ src/components/Answer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { FaGrinHearts } from "react-icons/fa";
+const Answer = () => {
+  return (
+    <div><h1>Ofcourse yes !!! <FaGrinHearts /></h1></div>
+  )
+}
+
+export default Answer
